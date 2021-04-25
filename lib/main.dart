9f2d7095e7cdc179f8120b5b7d394f0d821214b8
xanf-code <darshanaswath@gmail.com>
2021-04-25 19:51:32 +0530

    urlPrint

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,7 @@ class _HomeState extends State<Home> {
         String downloadUrl = await snapshot.ref.getDownloadURL();
         setState(() {
           imgUrl = downloadUrl;
+          debugPrint(imgUrl);
         });
       });
     });
